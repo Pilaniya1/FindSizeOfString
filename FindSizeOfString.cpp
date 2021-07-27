76ea0369d3dc52fd79ccc2989bd88344bbe9988b
Vinay Pilaniya <82079006+Pilaniya1@users.noreply.github.com>
2021-07-27 19:14:45 +0530

    Add files via upload
    ->Find size of the string by using array and char(Keyword).
    ->Output of the program is :- 6.

---
+++ FindSizeOfString.cpp
@@ -0,0 +1,13 @@
+#include<bits\stdc++.h>
+using namespace std;
+main(){
+    char s[]="String";
+    char* cp=s;
+    for(;*cp;cp++);
+    {  
+    int i=cp-s;
+    cout<<i;
+    }
+    return 0;
+}    
+   
